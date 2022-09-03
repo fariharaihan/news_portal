40e748e566baf3a05d0dc96c0d95a369294a5f88
fariharaihan <liafariha5@gmail.com>
2022-09-03 19:31:49 +0600

    news detail

--- js/news.js
+++ js/news.js
@@ -1,1 +1,73 @@
 
+
+const categoryLoadData = async () => {
+    const cateGoryUrl = `https://openapi.programming-hero.com/api/news/categories`;
+    // const cateGoryUrl = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
+    const res = await fetch(cateGoryUrl);
+    const data = await res.json();
+    displayCategory(data.data.news_category);
+}
+
+const displayCategory = allCategoris => {
+    // console.log(allCategoris)
+    const categoryContainer = document.getElementById('category-container');
+
+    categoryContainer.classList.add('list-item')
+    allCategoris.forEach(category => {
+        // console.log(category)
+        const categoryDiv = document.createElement('ul')
+        categoryDiv.innerHTML = `
+        <a onclick="categoryNewsloadData('${category.category_id}')">${category.category_name}</a>
+        `;
+
+        categoryContainer.appendChild(categoryDiv);
+    })
+}
+
+
+const categoryNewsloadData = async (_id) => {
+    // spinnerToggle(true);
+    const newsCategoryUrl = `https://openapi.programming-hero.com/api/news/category/${_id}`;
+    // const newsCategoryUrl = ` https://openapi.programming-hero.com/api/news/category/01`;
+    const res = await fetch(newsCategoryUrl);
+    const data = await res.json();
+    displayCategoryNews(data.data);
+}
+
+
+const displayCategoryNews = categoryItem => {
+    console.log(categoryItem)
+    const newsContainer = document.getElementById('news-container')
+    categoryItem.forEach(news => {
+        console.log(news)
+
+        const newsDiv = document.createElement('div');
+        newsDiv.classList.add('col');
+        newsDiv.innerHTML = `
+
+        <div class="card mb-3 ">
+        <div  class="row g-0">
+            <div class="col-md-2">
+                <img src="${news.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-10">
+                <div class="card-body">
+                    <h5 class="card-title">${news.title ? news.title : 'No title found'}</h5>
+                    <p class="card-text">${news.details.slice(0, 150)}</p>
+                     <div class=" d-flex aling-item-center justify-content-between">
+                        <div class="d-flex flex-column"><img src="${news.author.img}" class="img-thumbnail" alt=""><span class="px-3"style="width:100px">${news.author.name ? news.author.name : 'No data found'}</span></div>
+                        <div><i class="fa-regular fa-eye"></i>${news.total_view ? news.total_view : 'No viewer found'}</div>
+                        <button class="view-btn">View Details</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+           
+            `;
+        newsContainer.appendChild(newsDiv);
+    });
+}
+
+// loadCategory()
+categoryLoadData()
